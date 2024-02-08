ef883176ef43112aaa1e92caaec2b46d3b55cc77
Gabriele Bongio <bongiodieci@gmail.com>
2024-02-08 17:09:31 +0100

    Aggiornamento e sistemazione

--- SQLQueryEsercizio1.sql
+++ SQLQueryEsercizio1.sql
@@ -1,110 +1,32 @@
-CREATE PROCEDURE Esercizio_1_a
-AS
-BEGIN
-	SELECT * FROM Impiegati WHERE Età > 29
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_b
-AS
-BEGIN
-	SELECT * FROM Impiegati WHERE Reddito_Mensile > 8000
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_c
-AS
-BEGIN
-	SELECT * FROM Impiegati WHERE Detrazione_Fiscale = 1
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_d
-AS
-BEGIN
-	SELECT * FROM Impiegati WHERE Detrazione_Fiscale = 0
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_e
-AS
-BEGIN
-	SELECT * FROM Impiegati WHERE Cognome LIKE 'G%' ORDER BY Cognome
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_f
-AS
-BEGIN
-	SELECT COUNT(*) FROM Impiegati
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_g
-AS
-BEGIN
-	SELECT SUM(Reddito_Mensile) FROM Impiegati
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_h
-AS
-BEGIN
-	SELECT AVG(Reddito_Mensile) FROM Impiegati
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_i
-AS
-BEGIN
-	SELECT MAX(Reddito_Mensile) FROM Impiegati
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_j
-AS
-BEGIN
-	SELECT MIN(Reddito_Mensile) FROM Impiegati
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_k
-AS
-BEGIN
-	SELECT * FROM Impiegati INNER JOIN Impieghi 
-	ON Impiegati.IDImpiego = Impieghi.IDImpiego
-	--WHERE Data_Assunzione > '01/01/2007' AND Data_Assunzione < '01/01/2008'
-	WHERE Data_Assunzione BETWEEN '01/01/2007' AND '01/01/2008'
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_l(
-	@Tipo_Impiego NVARCHAR(100)
-	)
-AS
-BEGIN
-	SELECT * FROM Impiegati INNER JOIN Impieghi 
-	ON Impiegati.IDImpiego = Impieghi.IDImpiego
-	--WHERE Data_Assunzione > '01/01/2007' AND Data_Assunzione < '01/01/2008'
-	WHERE Tipo_Impiego = @Tipo_Impiego
-END
-GO
-
-
-CREATE PROCEDURE Esercizio_1_m
-AS
-BEGIN
-	SELECT AVG(Età) FROM Impiegati
-END
-GO
+-- Esercizio 1.a
+SELECT * FROM Impiegati WHERE Età > 29
+-- Esercizio 1.b
+SELECT * FROM Impiegati WHERE Reddito_Mensile > 8000
+-- Esercizio 1.c
+SELECT * FROM Impiegati WHERE Detrazione_Fiscale = 1
+-- Esercizio 1.d
+SELECT * FROM Impiegati WHERE Detrazione_Fiscale = 0
+-- Esercizio 1.e
+SELECT * FROM Impiegati WHERE Cognome LIKE 'G%' ORDER BY Cognome
+-- Esercizio 1.f
+SELECT COUNT(*) FROM Impiegati
+-- Esercizio 1.g
+SELECT SUM(Reddito_Mensile) FROM Impiegati
+-- Esercizio 1.h
+SELECT AVG(Reddito_Mensile) FROM Impiegati
+-- Esercizio 1.i
+SELECT MAX(Reddito_Mensile) FROM Impiegati
+-- Esercizio 1.j
+SELECT MIN(Reddito_Mensile) FROM Impiegati
+-- Esercizio 1.k
+SELECT * FROM Impiegati INNER JOIN Impieghi 
+ON Impiegati.IDImpiego = Impieghi.IDImpiego
+--WHERE Data_Assunzione > '01/01/2007' AND Data_Assunzione < '01/01/2008'
+WHERE Data_Assunzione BETWEEN '01/01/2007' AND '01/01/2008'
+-- Esercizio 1.l
+DECLARE @Tipo_Impiego NVARCHAR(100) = 'Dirigente'
+SELECT * FROM Impiegati INNER JOIN Impieghi 
+ON Impiegati.IDImpiego = Impieghi.IDImpiego
+WHERE Tipo_Impiego = @Tipo_Impiego
+-- Esercizio 1.m
+SELECT AVG(Età) FROM Impiegati
